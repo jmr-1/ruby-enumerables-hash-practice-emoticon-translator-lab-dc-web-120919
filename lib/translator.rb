9be0aa5cdd55f14e1f9cf620edce6804cbb65d1a
J <jmrumingan@gmail.com>
2019-10-29 14:44:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,12 +7,17 @@ def load_library(file)
   new_hash[:get_meaning] = {}
   new_hash[:get_emoticon] = {}
   
-  
-  emotes.each do |key, value|
-    #puts "#{key} and #{value}"
-    new_key = value[1]
+  #JP emoticons 
+  emotes.each do |key, value| #key = meaning, value = emoticon 
+    new_key = value[1] #hardcoded 1 due to the library format 
     n_hash = {new_key=>"#{key}"}
     new_hash[:get_meaning].merge!(n_hash)
+  end 
+  
+  #US emoticons 
+  emotes.each do |meaning, emoticons|
+    temp_hash = {emoticon[0]=>"#{meaning}"}
+    new_hash[:get_meaning].merge!(temp_hash)
   end 
   
   puts "new hash"
